47df6e6cdf2e2512d4a76bd50558090c206d2b8e
livhuwani98 <74908703+livhuwani98@users.noreply.github.com>
2022-04-02 09:36:16 +0200

    changes

--- src/test/java/ilAB/phoneNumber/phoneNumber.java
+++ src/test/java/ilAB/phoneNumber/phoneNumber.java
@@ -7,21 +7,21 @@ public class phoneNumber {
     public String number(){
 
 
-        Random number = new Random();
+        Random phoneN = new Random();
 
 
         int firstNum = 0;
         int[] carrierId = {6,7,8};
-        int afterCarrierId = number.nextInt(10);
-        int randomIndexForCarrierId = number.nextInt(carrierId.length);
+        int afterCarrierId = phoneN.nextInt(10);
+        int randomIndexForCarrierId = phoneN.nextInt(carrierId.length);
         String first3 = firstNum + "" + carrierId[randomIndexForCarrierId] + "" + afterCarrierId;
 
 
         int upperBoundForSecond3 = 1000;
-        int second3 =  number.nextInt(upperBoundForSecond3);
+        int second3 =  phoneN.nextInt(upperBoundForSecond3);
 
         int upperBoundForLast4 = 10000;
-        int last4 = number.nextInt(upperBoundForLast4);
+        int last4 = phoneN.nextInt(upperBoundForLast4);
 
 
         String phoneNum = first3 + " " + second3 + " " + last4;
